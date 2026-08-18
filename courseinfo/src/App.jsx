2bfc3935efb77613d69fd8aed7d4beab935bf946
Ana Laura Della Rosa <anadellarosa1@gmail.com>
2026-08-18 18:41:42 -0300

    edite el codigo 1.3 para resolver el ejercicio 1.4

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -8,20 +8,21 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    
     <div>
       <Parte 
-        name={props.parte1.name} //esta es la forma de llamar a una propiedad de un obejto 
-        ejercicios={props.parte1.exercises}
+        name={props.parts[0].name} 
+        ejercicios={props.parts[0].exercises}
       />
 
       <Parte
-        name={props.parte2.name}
-        ejercicios={props.parte2.exercises}
+        name={props.parts[1].name}
+        ejercicios={props.parts[1].exercises}
       />
 
       <Parte
-        name={props.parte3.name}
-        ejercicios={props.parte3.exercises}
+        name={props.parts[2].name}
+        ejercicios={props.parts[2].exercises}
       />
     </div>
   )
@@ -29,14 +30,16 @@ const Content = (props) => {
 
 const Parte = (props) => {
  return (  
-<p>La parte  se llama {props.name} y tiene un total de ejercicios {props.ejercicios}</p>
+  <p>
+  La parte  se llama {props.name} y tiene un total de ejercicios {props.ejercicios}
+  </p>
 )  
 }
 
 const Total = (props) => {
   return (
     <div>
-      <p>El numero total de ejercicios es {props.total} </p>
+      <p>El numero total de ejercicios es {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises} </p>
     </div>
   )
 }
@@ -44,36 +47,29 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-//Lo que hacemos aqui a diferencia del anterior es convertir en objetos
-//Objetos en JavaScript
-  const part1 = {
+//Ahora lo que hacemos es agrupar los objetos en un array 
+const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
 
   return (
     <div>
       <Header name={course} />
 
-      <Content
-        parte1={part1} //aqui llamas a toda la caja que contiene el objeto part1
-        parte2={part2}
-        parte3={part3}
-      />
+      <Content parts={parts} />
 
-      <Total
-        total={part1.exercises + part2.exercises + part3.exercises}
-      />
+      <Total parts={parts} />
     </div>
   )
 }

8fcd580a7858ccbf61c9d44072018284ebb37077
Ana Laura Della Rosa <anadellarosa1@gmail.com>
2026-08-19 12:42:52 -0300

    edite el ejercicio 1.4 para hacer el 1.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -46,30 +46,31 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-//Ahora lo que hacemos es agrupar los objetos en un array 
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-]
 
   return (
     <div>
-      <Header name={course} />
+      <Header name={course.name} />
 
-      <Content parts={parts} />
+      <Content parts={course.parts} />
 
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
